15fb25e6f184da52a7739f453865510ef84d567b
kspicer24 <kristispicer124@gmail.com>
2022-04-20 22:56:16 -0400

    added vendors and basic route

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import {BrowserRouter, Routes, Redirect, Route} from "react-router-dom";
+import HomeScreen from "./components/home_screen";
+import {Fragment} from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Fragment>
+            <Routes>
+              <Route exact path="/" element={<HomeScreen/>}/>
+            </Routes>
+        </Fragment>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/home_screen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomeComponent from "./home_component";
+
+
+const Index = () => {
+    return (
+        <div>
+            <HomeComponent/>
+        </div>
+    );
+}
+
+export default Index;
